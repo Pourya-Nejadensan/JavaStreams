794d9689c1ec5fb583fd385ec7436b065e85e308
Pourya <pourya.nejadensan@gmail.com>
2024-07-12 11:27:04 +0200

    Step 2: Use 'map' and double each number in the list.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,13 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         List<Integer> integerList = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
         integerList.stream().filter(integer -> integer % 2 == 0).forEach(System.out::println);
+
+        System.out.println("Step 2 ------------------------------------------------");
+
+        integerList.stream().map(integer -> integer * 2).forEach(System.out::println);
     }
 }
